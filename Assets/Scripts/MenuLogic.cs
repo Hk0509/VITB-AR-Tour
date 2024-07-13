0e415f3466e15721fd07a5d8cec744c68b77fa3f
Hk0509 <himanshukapoor898@gmail.com>
2024-07-13 19:50:49 +0530

    Apk built

---
+++ Assets/Scripts/MenuLogic.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuLogic : MonoBehaviour
+{
+  public void ExitButton()
+  {
+    Application.Quit();
+  }
+  public void PlayButton()
+  {
+      SceneManager.LoadScene("SampleScene");
+  }
+  public void AcademicButton()
+  {
+    SceneManager.LoadScene("AcademicBlock");
+  }
+  public void BoysHostelButton()
+  {
+    SceneManager.LoadScene("BoysHostel");
+  }
+  public void GirlsHostelButton()
+  {
+    SceneManager.LoadScene("GirlsHostel");
+  }
+  public void BoysHostelB2Button()
+  {
+    SceneManager.LoadScene("BoysHostelB2");
+  }
+  public void LabComplexButton()
+  {
+    SceneManager.LoadScene("LabComplex");
+  }
+  public void MulriPurposeButton()
+  {
+    SceneManager.LoadScene("MultipurposeHall");
+  }
+  public void MenuButton()
+  {
+    SceneManager.LoadScene("MainMenuScene");
+  }
+}
+    
